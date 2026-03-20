233532c385cc046fa63d568df57f02b6e6c648f8
VinceV0 <30036567@student.toiohomai.ac.nz>
2026-03-21 10:24:16 +1300

    Fixing Deployment Loading

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/React-SCP-Assignment1/', // This MUST match your repo name exactly
+  plugins: [react()]
 })
